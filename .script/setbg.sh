23242ab028b66ec87c471eba6c1e9720e0882ce4
unt32 <vladsaen5@gmail.com>
2026-04-07 21:47:50 +0300

    moved startup to services; +random wallpaper

---
+++ .script/setbg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
+
+if [ ! -d "$WALLPAPER_DIR" ]; then
+	echo "Error: Wallpaper directory '$WALLPAPER_DIR' does not exist."
+	exit 1
+fi
+
+
+IMAGE_PATH=$(find "$WALLPAPER_DIR" -type f -iregex '.*\.\(jpg\|jpeg\|png\|gif\|webp\)$' | shuf -n 1)
+
+if [ -z "$IMAGE_PATH" ]; then
+	echo "Error: No images found in '$WALLPAPER_DIR'."
+	exit 1
+fi
+
+echo "Setting background to: $IMAGE_PATH"
+
+
+swaybg -i "$IMAGE_PATH" -m fill
--- .vimrc
+++ .vimrc
@@ -1,7 +1,12 @@
 set number
 set relativenumber
+
 syntax on
 filetype plugin indent on
+
 set termguicolors
 colorscheme torte
 highlight Normal guibg=NONE ctermbg=NONE
+
+command! Ru set keymap=russian-jcukenwin
+command! En set keymap=
