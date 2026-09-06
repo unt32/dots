51d4f722b47f4a61db9a8c7c776e275d5a74e18d
unt32 <vladsaen5@gmail.com>
2026-09-06 18:28:27 +0300

    some updates

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "catppuccin", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
+  { "mfussenegger/nvim-dap", enabled = false },
+  { "mfussenegger/nvim-dap-python", enabled = false },
 }
